2996b6f3f6c25c8fcc6d1f5a88c2eaf3b885b7e4
Fabrice Beya <byxfab001@myuct.ac.za>
2018-09-01 06:29:19 +0200

    Added user authentification
    Created basic user model.
    Added ability for user to register and login using passport.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    seedDB      = require("./seed.js"),
-    assetRoutes = require("./routes/assets"),
-    indexRoutes = require("./routes/index"),
-    transactionRoutes = require("./routes/transactions");
+var express             = require("express"),
+    app                 = express(),
+    bodyParser          = require("body-parser"),
+    mongoose            = require("mongoose"),
+    seedDB              = require("./seed.js"),
+    assetRoutes         = require("./routes/assets"),
+    indexRoutes         = require("./routes/index"),
+    transactionRoutes   = require("./routes/transactions"),
+    passport            = require("passport"),
+    LocalStrategy       = require("passport-local"),
+    User                = require("./models/user")
     
 //seedDB();
+
 mongoose.connect("mongodb://localhost/tokenbar");
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -16,6 +20,25 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+//Configure Passport
+app.use(require("express-session")({
+    secret : "Sight of Light too Bright to Fight",
+    resave : false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy( User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//push to every page
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
+
+//configure routes
 app.use("/", indexRoutes);
 app.use("/assets", assetRoutes);
 app.use("/transactions", transactionRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express     = require("express"),
-    router      = express.Router()
+    router      = express.Router(),
+    User = require("../models/user"),
+    passport = require("passport")
 
 //Landing
 router.get("/", function(req, res){
@@ -9,6 +11,54 @@ router.get("/", function(req, res){
 //FAQ
 router.get("/about", function(req, res){
     res.render("about");
+});
+
+//Show register form
+router.get("/register", function(req, res){
+    res.render("register");
 })
+
+//Register a new user
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.redirect("/register");
+        } else {
+            //log user in
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/assets");
+            })
+        }
+    });
+})
+
+//show login page
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+//authenticate a user
+router.post("/login", passport.authenticate("local",
+{
+    successRedirect: "/",
+    failureRedirect: "/login"
+
+}) ,function(req, res){})
+
+//logout
+router.get("/logout", function(req, rest){
+    req.logOut();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        res.redirect("/login");
+    }
+}
 
 module.exports = router;
