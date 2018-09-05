e47a020f461c607067e371859b809e24778349c6
Fabrice BEYA <byxfab001@myuct.ac.za>
2018-09-05 18:00:45 +0200

    Added new content
    Added an about adn faq page. Added more content to landing page.

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ router.get("/", function(req, res){
     res.render("landing");
 });
 
-//FAQ
+//about
 router.get("/about", function(req, res){
     res.render("about");
+});
+//FAQ
+router.get("/faq", function(req, res){
+    res.render("faq");
 });
 
 module.exports = router;
