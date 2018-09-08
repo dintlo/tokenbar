7c4103ff0534ac7dd8cdd11f5ce225bec2a6d270
Fabrice Beya <byxfab001@myuct.ac.za>
2018-09-08 12:05:19 +0200

    Bug fix
    Show route must go before edit

--- routes/assets.js
+++ routes/assets.js
@@ -30,6 +30,18 @@ router.get("/new",middlewareObj.isLoggedIn, function(req, res){
     res.render("assets/new");
 })
 
+//Asset:Show
+router.get("/:id", function(req, res){
+    Asset.findById(req.params.id).populate("transactions","wallet").exec(function(err, asset){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("assets/show", {asset: asset})
+        }
+    })
+})
+
+
 //Asset:edit
 router.get("/:id/edit",middlewareObj.isAssetCreator, function(req, res){
     Asset.findById(req.params.id, function(err,foundAsset){
@@ -43,16 +55,6 @@ router.get("/:id/edit",middlewareObj.isAssetCreator, function(req, res){
     
 })
 
-//Asset:Show
-router.get("/:id", function(req, res){
-    Asset.findById(req.params.id).populate("transactions","wallet").exec(function(err, asset){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("assets/show", {asset: asset})
-        }
-    })
-})
 
 //Asset:Put
 router.put("/:id", middlewareObj.isAssetCreator, function(req, res){
